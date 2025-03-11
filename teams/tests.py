6a8ec1b4b73dba39c0b0d8f559bdd5d6ea388979
mkrajacic <63226581+mkrajacic@users.noreply.github.com>
2025-03-11 19:50:26 +0100

    Add tests

--- teams/tests.py
+++ teams/tests.py
@@ -1,3 +1,40 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Team, Singer, TeamMember
+from django.contrib.auth import get_user_model
+User=get_user_model()
+from django.conf import settings
 
-# Create your tests here.
+class TeamModelTests(TestCase):
+    def test_surpassed_member_limit(self):
+        user = add_user("mk")
+        team = add_team("test team", user.id)
+        for x in range(1,7):
+            singer = add_singer(f"singer {x}", f"song {x}")
+            add_team_member(team.id, singer.id)
+
+        self.assertTrue(team.reached_member_limit())
+
+    def test_below_member_limit(self):
+        user = add_user("mk")
+        team = add_team("test team", user.id)
+        for x in range(1,4):
+            singer = add_singer(f"singer {x}", f"song {x}")
+            add_team_member(team.id, singer.id)
+
+        self.assertFalse(team.reached_member_limit())
+        
+
+def add_user(username):
+    return User.objects.create(username=username)
+
+def add_team(name, user):
+    return Team.objects.create(name=name, user_id=user)
+
+def add_singer(name, song):
+    return Singer.objects.create(name=name, song=song)
+
+def add_team_member(team_id, singer_id):
+    return TeamMember.objects.create(team_id=team_id, singer_id=singer_id)
