198df9a8734ec21261b00b49aace3a636d2f4a61
mkrajacic <63226581+mkrajacic@users.noreply.github.com>
2025-03-11 20:01:19 +0100

    Add common file for test functions

--- teams/tests.py
+++ teams/tests.py
@@ -2,39 +2,24 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
-from .models import Team, Singer, TeamMember
-from django.contrib.auth import get_user_model
-User=get_user_model()
 from django.conf import settings
+from common import TestFunctions
 
 class TeamModelTests(TestCase):
     def test_surpassed_member_limit(self):
-        user = add_user("mk")
-        team = add_team("test team", user.id)
+        user = TestFunctions.add_user("mk")
+        team = TestFunctions.add_team("test team", user.id)
         for x in range(1,7):
-            singer = add_singer(f"singer {x}", f"song {x}")
-            add_team_member(team.id, singer.id)
+            singer = TestFunctions.add_singer(f"singer {x}", f"song {x}")
+            TestFunctions.add_team_member(team.id, singer.id)
 
         self.assertTrue(team.reached_member_limit())
 
     def test_below_member_limit(self):
-        user = add_user("mk")
-        team = add_team("test team", user.id)
+        user = TestFunctions.add_user("mk")
+        team = TestFunctions.add_team("test team", user.id)
         for x in range(1,4):
-            singer = add_singer(f"singer {x}", f"song {x}")
-            add_team_member(team.id, singer.id)
+            singer = TestFunctions.add_singer(f"singer {x}", f"song {x}")
+            TestFunctions.add_team_member(team.id, singer.id)
 
         self.assertFalse(team.reached_member_limit())
-        
-
-def add_user(username):
-    return User.objects.create(username=username)
-
-def add_team(name, user):
-    return Team.objects.create(name=name, user_id=user)
-
-def add_singer(name, song):
-    return Singer.objects.create(name=name, song=song)
-
-def add_team_member(team_id, singer_id):
-    return TeamMember.objects.create(team_id=team_id, singer_id=singer_id)
--- users/tests.py
+++ users/tests.py
@@ -2,35 +2,20 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
-from teams.models import Team, Singer, TeamMember
-from django.contrib.auth import get_user_model
-User=get_user_model()
 from django.conf import settings
+from common import TestFunctions
 
 class UserModelTests(TestCase):
     def test_surpassed_team_limit(self):
-        user = add_user("mk")
+        user = TestFunctions.add_user("mk")
         for x in range(1,7):
-            add_team(f"team {x} name", user.id)
+            TestFunctions.add_team(f"team {x} name", user.id)
 
         self.assertTrue(user.reached_team_limit())
 
     def test_below_team_limit(self):
-        user = add_user("mk")
+        user = TestFunctions.add_user("mk")
         for x in range(1,4):
-            add_team(f"team {x} name", user.id)
+            TestFunctions.add_team(f"team {x} name", user.id)
 
         self.assertFalse(user.reached_team_limit())
-        
-
-def add_user(username):
-    return User.objects.create(username=username)
-
-def add_team(name, user):
-    return Team.objects.create(name=name, user_id=user)
-
-def add_singer(name, song):
-    return Singer.objects.create(name=name, song=song)
-
-def add_team_member(team_id, singer_id):
-    return TeamMember.objects.create(team_id=team_id, singer_id=singer_id)
