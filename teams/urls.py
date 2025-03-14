36aa070eaafc69a19e8d715c8fe7647d7130ed6d
mkrajacic <matijaleader5@gmail.com>
2025-03-14 22:30:43 +0100

    Prepare teams index view

--- teams/urls.py
+++ teams/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "teams"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.ShowTeams.as_view(), name="index"),
     path("<int:pk>/", views.ViewTeam.as_view(), name="view-team"),
     #path("<int:pk>/modify", views.ModifyTeam.as_view(), name="modify-team"),
     #path("<int:pk>/modify-lineup", views.ModifyTeamLineup.as_view(), name="modify-team-lineup"),
--- teams/views.py
+++ teams/views.py
@@ -2,16 +2,18 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Team
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
-class IndexView(generic.ListView):
-    template_name = "teams/index.html"
-    context_object_name = "teams_by_user_list"
+class ShowTeams(LoginRequiredMixin, generic.ListView):
+    template_name = "index.html"
+    context_object_name = "user_teams"
 
     def get_queryset(self):
-        #return Team.objects.filter(data_pub__lte=timezone.now(),scelta__isnull=False).distinct().order_by("-data_pub")[:5]
-        return "ecco"
+        return self.request.user.team_set.all()
     
 class ViewTeam(generic.DetailView):
     model = Team
@@ -20,20 +22,3 @@ class ViewTeam(generic.DetailView):
     def get_queryset(self):
         #return Team.objects.filter(data_pub__lte=timezone.now(), scelta__isnull=False).distinct()
         return "ecco"
-
-""""
-def vota(richiesta, domanda_id):
-    domanda = get_object_or_404(Team, pk=domanda_id)
-
-    try:
-        fatta_scelta = domanda.scelta_set.get(pk=richiesta.POST['scelta'])
-    except (KeyError, Scelta.DoesNotExist):
-        return render(richiesta, "teams/view-team.html", {
-            "domanda": domanda,
-            "messagio_error": "Non hai selezionato una scelta"
-        })
-    else:
-        fatta_scelta.voti = F("voti") + 1
-        fatta_scelta.save()
-        return HttpResponseRedirect(reverse("polls:risultati", args=(domanda.id,)))
-"""
