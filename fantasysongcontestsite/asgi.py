bfa5997e13006997288beff2b6fa6a36fddd0037
mkrajacic <63226581+mkrajacic@users.noreply.github.com>
2025-04-11 13:19:18 +0200

    Rename project files

--- fantasysongcontestsite/asgi.py
+++ fantasysongcontestsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fantabuildersite project.
+ASGI config for fantasysongcontestsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantabuildersite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasysongcontestsite.settings")
 
 application = get_asgi_application()
--- fantasysongcontestsite/wsgi.py
+++ fantasysongcontestsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fantabuildersite project.
+WSGI config for fantasysongcontestsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantabuildersite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasysongcontestsite.settings")
 
 application = get_wsgi_application()
